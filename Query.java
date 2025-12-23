fe8aa47c41f9f7c39990e7cfb91d0ceb777383dc
JohnReard <230427826@aston.ac.uk>
2025-12-23 22:37:15 +0000

    Delete Query.java

--- Query.java
+++
@@ -1,9 +0,0 @@
-public class Query{
-    String queryMessage;
-    Boolean response;
-    public Query(String queryMessage){
-        this.queryMessage = queryMessage;
-        this.response = false;
-    }
-    
-}
